ba2f217943cbd7019c1bdc8ff86d1ee0b80ed7e0
AdimirTen <adimirten@gmail.com>
2024-09-28 13:34:00 +0600

    loops for

---
+++ script.js
@@ -0,0 +1,86 @@
+// function sum (a,b) {
+// console.log(a + b)
+// }
+
+// sum(2,5)
+
+
+// let num1 = 10
+// let num2 = 9
+
+// function findBigger(a,b) {
+//     return a > b
+// }
+
+// if (findBigger(num1,num2)) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+ 
+// function showMessage(message = "Default message") {
+//     console.log(message);
+// }
+
+// function findDivision(a,b) {
+//     console.log(a/b)
+// }
+
+// findDivision(8,3)
+
+// function findSquare(c) {
+//     console.log(c*c)
+// }
+
+// findSquare(5)
+
+// function findSum(a,b,c,d,e,) {
+//     console.log(a+b+c+d+e)
+// }
+
+// findSum(1,5,5,4,9)
+
+let string = "Marocco"
+
+function findLength() {
+console.log(string.length)
+}
+
+findLength()
+
+// let a= -7
+
+// function findAbsolute(a){
+// return Math.abs(a)
+// }
+
+
+
+function findLengthOfString(array = ["snjfvnswvn","ojvhjwoifijwqf","visfgpnasfw"]) {
+return array.map(str => str.length)
+}
+
+let array = [2,5,7,6,7]
+function double(array) {
+return array.map(str => str * 2)
+}
+
+let a = 4
+
+function findChet(a) {
+    return a
+}
+ 
+if (a%2 === 0) {
+    console.log("Chetnoe")
+}
+else {
+    console.log("nechetnoe")
+}
+
+
+
+
+
+
